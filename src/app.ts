63caafc7f113c9f77839988e41f3cdd668f52b85
Issei Mori <isseieikisouya@outlook.jp>
2023-01-18 15:58:58 +0900

    use fs and readed file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import * as http from 'http';
+import * as fs from 'fs';
 
 
 const port = 3000;
+const fileName : string = './src/test.html'
+//あくまでカレントディレクトリはランタイムの実行環境がいるとことになるから、ルートから実行した場合src/test.htmlになる
 
 const server = http.createServer(
   (request : http.IncomingMessage, response : http.ServerResponse<http.IncomingMessage>) => {
-    response.writeHead(200,  'oh yeah');
-    response.write('you can write body');
-    response.end("Hello! Node.js with TypeScript");
+    fs.readFile(fileName, 'utf8', (err, data)=> {
+        console.log(err);
+        console.log(data);
+        response.write(data);
+        response.end();
+    });
+    
   }
 );
 server.listen(port);
