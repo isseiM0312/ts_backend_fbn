c3a8c176679bd002b2cbafbb61301776594bbd02
Issei Mori <isseieikisouya@outlook.jp>
2023-01-18 15:28:23 +0900

    edit header and add html file

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ const port = 3000;
 
 const server = http.createServer(
   (request : http.IncomingMessage, response : http.ServerResponse<http.IncomingMessage>) => {
+    response.writeHead(200,  'oh yeah');
+    response.write('you can write body');
     response.end("Hello! Node.js with TypeScript");
   }
 );
