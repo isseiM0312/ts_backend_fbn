6e0e2cd9c63153c5f3f4b2d3a2190b65f9841a3b
Issei Mori <isseieikisouya@outlook.jp>
2023-01-18 15:17:03 +0900

    deleted unneeded comments

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import * as http from 'http';
 
 
-const port = 3000; // ポート番号
+const port = 3000;
 
-// httpサーバーを設定する
 const server = http.createServer(
   (request : http.IncomingMessage, response : http.ServerResponse<http.IncomingMessage>) => {
-    // サーバーにリクエストがあった時に実行される関数
     response.end("Hello! Node.js with TypeScript");
   }
 );
-// サーバーを起動してリクエストを待ち受け状態にする
 server.listen(port);
-// ログを出力する
